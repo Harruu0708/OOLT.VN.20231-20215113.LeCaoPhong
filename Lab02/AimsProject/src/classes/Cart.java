ce5f75e34af60320d414771384f43a6add763e2b
Cao Phong <133210829+Harruu0708@users.noreply.github.com>
2023-10-27 23:45:40 +0700

    Update

--- Lab02/AimsProject/src/classes/Cart.java
+++ Lab02/AimsProject/src/classes/Cart.java
@@ -1,5 +1,4 @@
 package Classes;
-import java.util.ArrayList;
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
